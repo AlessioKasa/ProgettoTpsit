8407a7c6899fb6a20b2a0e7b02222269a39ff30e
alessandromilli <alessandromilli2003@yahoo.it>
2021-11-30 11:22:59 +0100

    milli

--- app/src/main/java/com/example/progettotpsit/MainActivity.java
+++ app/src/main/java/com/example/progettotpsit/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.progettotpsit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"Soy milli",Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
     }
 }
